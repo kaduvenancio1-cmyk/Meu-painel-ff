e55249b31eee85561f4b398788aa3dce9e4b9b7e
kaduvenancio1-cmyk <kaduvenancio1@gmail.com>
2026-01-23 04:08:13 -0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,39 +1,17 @@
 #import <UIKit/UIKit.h>
-#import <Security/Security.h>
 
 @interface RickzzMenu : UIView <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UIView *container;
 @property (nonatomic, strong) UITableView *tabela;
 @property (nonatomic, strong) NSArray *itens;
 @property (nonatomic, strong) UISlider *fovSlider;
+@property (nonatomic, strong) CAShapeLayer *fovCircle; // O Círculo real
 @end
 
 @implementation RickzzMenu
 
-// LIMPEZA PROFUNDA DE CONTA (GUEST RESET)
-static void reset_guest_account() {
-    // 1. Apaga todas as chaves de segurança do jogo (Keychain)
-    NSArray *secClasses = @[(__bridge id)kSecClassGenericPassword, (__bridge id)kSecClassInternetPassword, (__bridge id)kSecClassCertificate, (__bridge id)kSecClassKey, (__bridge id)kSecClassIdentity];
-    for (id secClass in secClasses) {
-        NSDictionary *spec = @{(__bridge id)kSecClass: secClass};
-        SecItemDelete((__bridge CFDictionaryRef)spec);
-    }
-    
-    // 2. Apaga arquivos de identificação na pasta Documents
-    NSFileManager *fm = [NSFileManager defaultManager];
-    NSString *docPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
-    NSArray *files = @[@"com.garena.msdk", @"device_id.txt", @"guest_info.json"];
-    for (NSString *file in files) {
-        [fm removeItemAtPath:[docPath stringByAppendingPathComponent:file] error:nil];
-    }
-    NSLog(@"[Rickzz] Limpeza profunda realizada.");
-}
-
 __attribute__((constructor))
-static void init_pro() {
-    // Executa a limpeza assim que o jogo abre
-    reset_guest_account();
-
+static void init_v8() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = [[UIApplication sharedApplication] keyWindow];
         UILongPressGestureRecognizer *segurar = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(aoSegurar:)];
@@ -61,39 +39,59 @@ static void init_pro() {
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.itens = @[@"Aimbot", @"ESP", @"Tracer", @"Skeleton", @"Distancia", @"Caixa 2D", @"Cabeça", @"Peito", @"Alerta Spect", @"Anti-Screenshot", @"Modo Streamer", @"ATIVAR/DESATIVAR FOV"];
+        self.itens = @[@"Aimbot", @"ESP", @"Tracer", @"Skeleton", @"Distancia", @"Caixa 2D", @"Cabeça", @"Peito", @"Alerta Spect", @"Anti-Screenshot", @"Modo Streamer", @"ATIVAR FOV"];
         
+        // Círculo do FOV (Invisível no início)
+        _fovCircle = [CAShapeLayer layer];
+        _fovCircle.strokeColor = [UIColor cyanColor].CGColor;
+        _fovCircle.fillColor = [UIColor clearColor].CGColor;
+        _fovCircle.lineWidth = 1.0;
+        _fovCircle.hidden = YES;
+        [self.layer addSublayer:_fovCircle];
+
+        // Menu Deitado
         _container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 450, 280)];
         _container.center = self.center;
         _container.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.95];
         _container.layer.cornerRadius = 10;
-        _container.layer.borderWidth = 1.5;
         _container.layer.borderColor = [UIColor cyanColor].CGColor;
+        _container.layer.borderWidth = 1.5;
         [self addSubview:_container];
 
-        UILabel *titulo = [[UILabel alloc] initWithFrame:CGRectMake(0, 5, 450, 25)];
-        titulo.text = @"RICKZZ PRO V7 - DEEP CLEAN";
-        titulo.textColor = [UIColor cyanColor];
-        titulo.textAlignment = NSTextAlignmentCenter;
-        titulo.font = [UIFont boldSystemFontOfSize:16];
-        [_container addSubview:titulo];
-
-        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(10, 35, 430, 180)];
+        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(10, 40, 430, 180)];
         _tabela.backgroundColor = [UIColor clearColor];
         _tabela.delegate = self;
         _tabela.dataSource = self;
         _tabela.separatorStyle = UITableViewCellSeparatorStyleNone;
         [_container addSubview:_tabela];
 
-        _fovSlider = [[UISlider alloc] initWithFrame:CGRectMake(50, 220, 350, 30)];
-        _fovSlider.minimumValue = 30;
-        _fovSlider.maximumValue = 150;
+        _fovSlider = [[UISlider alloc] initWithFrame:CGRectMake(50, 230, 350, 30)];
+        _fovSlider.minimumValue = 50;
+        _fovSlider.maximumValue = 300;
+        [_fovSlider addTarget:self action:@selector(fovAjuste:) forControlEvents:UIControlEventValueChanged];
         _fovSlider.hidden = YES;
         [_container addSubview:_fovSlider];
     }
     return self;
 }
 
+- (void)fovAjuste:(UISlider *)sender {
+    float r = sender.value;
+    CGPoint center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:r startAngle:0 endAngle:2*M_PI clockwise:YES];
+    _fovCircle.path = path.CGPath;
+}
+
+- (void)mudouSwitch:(UISwitch *)sender {
+    NSString *opt = self.itens[sender.tag];
+    if ([opt isEqualToString:@"ATIVAR FOV"]) {
+        _fovCircle.hidden = !sender.on;
+        _fovSlider.hidden = !sender.on;
+        [self fovAjuste:_fovSlider]; // Desenha ao ligar
+    }
+}
+
+// Métodos da Tabela (Obrigatórios)
 - (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)s { return self.itens.count; }
 - (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)ip {
     UITableViewCell *c = [tv dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"c"];
@@ -102,15 +100,9 @@ static void init_pro() {
     c.textLabel.textColor = [UIColor whiteColor];
     UISwitch *s = [[UISwitch alloc] init];
     s.tag = ip.row;
-    s.transform = CGAffineTransformMakeScale(0.7, 0.7);
     [s addTarget:self action:@selector(mudouSwitch:) forControlEvents:64];
     c.accessoryView = s;
     return c;
 }
 
-- (void)mudouSwitch:(UISwitch *)sender {
-    if ([self.itens[sender.tag] isEqualToString:@"ATIVAR/DESATIVAR FOV"]) {
-        _fovSlider.hidden = !sender.on;
-    }
-}
 @end
