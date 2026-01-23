a4ea60566470ae07c97d3537563cc1be04f188f0
kaduvenancio1-cmyk <kaduvenancio1@gmail.com>
2026-01-23 05:31:54 -0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -2,10 +2,10 @@
 #import <Security/Security.h>
 #import <mach-o/dyld.h>
 
-// Variáveis de ativação para o Contra Squad
-static bool espAtivo = false;
+// Variáveis de controle para o CS
 static bool aimAtivo = false;
-static int modoTiro = 0; // 0 para Cabeça, 1 para Peito
+static bool espAtivo = false;
+static int localAlvo = 0; 
 
 @interface RickzzMenu : UIView <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UIView *container;
@@ -17,41 +17,41 @@ static int modoTiro = 0; // 0 para Cabeça, 1 para Peito
 
 @implementation RickzzMenu
 
-// LIMPEZA DE CONTA (GUEST RESET)
-static void deepClean() {
-    NSString *guest = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/com.garena.msdk/guest.dat"];
-    [[NSFileManager defaultManager] removeItemAtPath:guest error:nil];
+// LIMPEZA AUTOMÁTICA DE CONTA
+static void resetApp() {
+    NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/com.garena.msdk/guest.dat"];
+    [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
     SecItemDelete((__bridge CFDictionaryRef)@{(__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword});
 }
 
 __attribute__((constructor))
-static void start() {
-    deepClean();
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+static void load() {
+    resetApp();
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = [[UIApplication sharedApplication] keyWindow];
-        UILongPressGestureRecognizer *seg = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(abrirPainel:)];
-        seg.numberOfTouchesRequired = 3;
-        seg.minimumPressDuration = 2.0;
-        [win addGestureRecognizer:seg];
+        UILongPressGestureRecognizer *hold = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(show:)];
+        hold.numberOfTouchesRequired = 3;
+        hold.minimumPressDuration = 2.0;
+        [win addGestureRecognizer:hold];
     });
 }
 
-+ (void)abrirPainel:(UILongPressGestureRecognizer *)g {
-    if (g.state == UIGestureRecognizerStateBegan) [self alternar];
++ (void)show:(UILongPressGestureRecognizer *)g {
+    if (g.state == UIGestureRecognizerStateBegan) [self toggle];
 }
 
-+ (void)alternar {
++ (void)toggle {
     UIWindow *win = [[UIApplication sharedApplication] keyWindow];
-    UIView *p = [win viewWithTag:999];
-    if (p) [p removeFromSuperview];
+    UIView *old = [win viewWithTag:999];
+    if (old) [old removeFromSuperview];
     else {
         RickzzMenu *menu = [[RickzzMenu alloc] initWithFrame:win.bounds];
         menu.tag = 999;
-        menu.layer.zPosition = 9999;
         [win addSubview:menu];
     }
 }
 
+// CORREÇÃO DO ERRO DE INICIALIZAÇÃO (NS_DESIGNATED_INITIALIZER)
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
@@ -68,55 +68,58 @@ static void start() {
         _container.center = self.center;
         _container.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.95];
         _container.layer.cornerRadius = 15;
-        _container.layer.borderWidth = 1.2;
+        _container.layer.borderWidth = 1.0;
         _container.layer.borderColor = [UIColor cyanColor].CGColor;
         [self addSubview:_container];
 
-        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(10, 40, 400, 160)];
+        // CORREÇÃO DA TABELA
+        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(10, 40, 400, 160) style:UITableViewStylePlain];
         _tabela.backgroundColor = [UIColor clearColor];
-        _tabela.delegate = self; _tabela.dataSource = self;
-        _tabela.separatorStyle = 0;
+        _tabela.delegate = self;
+        _tabela.dataSource = self;
+        _tabela.separatorStyle = UITableViewCellSeparatorStyleNone;
         [_container addSubview:_tabela];
 
         _fovSlider = [[UISlider alloc] initWithFrame:CGRectMake(40, 210, 340, 30)];
-        _fovSlider.minimumValue = 50; _fovSlider.maximumValue = 400;
-        [_fovSlider addTarget:self action:@selector(fovSet:) forControlEvents:64];
+        _fovSlider.minimumValue = 50; _fovSlider.maximumValue = 350;
+        [_fovSlider addTarget:self action:@selector(ajuste:) forControlEvents:UIControlEventValueChanged];
         _fovSlider.hidden = YES;
         [_container addSubview:_fovSlider];
     }
     return self;
 }
 
-- (void)fovSet:(UISlider *)s {
+- (void)ajuste:(UISlider *)s {
     UIBezierPath *p = [UIBezierPath bezierPathWithArcCenter:CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2) radius:s.value startAngle:0 endAngle:2*M_PI clockwise:YES];
     _fovCircle.path = p.CGPath;
 }
 
-- (void)mudouSwitch:(UISwitch *)sw {
-    NSString *opt = self.itens[sw.tag];
-    if ([opt isEqualToString:@"Aimbot Pro"]) aimAtivo = sw.on;
-    if ([opt containsString:@"ESP"]) espAtivo = sw.on;
-    if ([opt isEqualToString:@"Cabeça"] && sw.on) { modoTiro = 0; [self off:6]; }
-    if ([opt isEqualToString:@"Peito"] && sw.on) { modoTiro = 1; [self off:5]; }
+- (void)mudou:(UISwitch *)s {
+    NSString *opt = self.itens[s.tag];
+    if ([opt isEqualToString:@"Aimbot Pro"]) aimAtivo = s.on;
+    if ([opt containsString:@"ESP"]) espAtivo = s.on;
+    if ([opt isEqualToString:@"Cabeça"] && s.on) { localAlvo = 0; [self off:6]; }
+    if ([opt isEqualToString:@"Peito"] && s.on) { localAlvo = 1; [self off:5]; }
     if ([opt isEqualToString:@"ATIVAR FOV"]) {
-        _fovCircle.hidden = !sw.on; _fovSlider.hidden = !sw.on;
-        [self fovSet:_fovSlider];
+        _fovCircle.hidden = !s.on; _fovSlider.hidden = !s.on;
+        [self ajuste:_fovSlider];
     }
 }
 
-- (void)off:(int)i {
-    UITableViewCell *c = [self.tabela cellForRowAtIndexPath:[NSIndexPath indexPathForRow:i inSection:0]];
+- (void)off:(int)idx {
+    UITableViewCell *c = [self.tabela cellForRowAtIndexPath:[NSIndexPath indexPathForRow:idx inSection:0]];
     if (c) [(UISwitch *)c.accessoryView setOn:NO animated:YES];
 }
 
 - (NSInteger)tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)s { return self.itens.count; }
 - (UITableViewCell *)tableView:(UITableView *)t cellForRowAtIndexPath:(NSIndexPath *)ip {
-    UITableViewCell *c = [t dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:0 reuseIdentifier:@"c"];
+    UITableViewCell *c = [t dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"c"];
     c.backgroundColor = [UIColor clearColor];
     c.textLabel.text = self.itens[ip.row];
     c.textLabel.textColor = [UIColor whiteColor];
     UISwitch *sw = [[UISwitch alloc] init];
-    sw.tag = ip.row; [sw addTarget:self action:@selector(mudouSwitch:) forControlEvents:64];
+    sw.tag = ip.row;
+    [sw addTarget:self action:@selector(mudou:) forControlEvents:UIControlEventValueChanged];
     c.accessoryView = sw;
     return c;
 }
