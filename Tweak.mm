1f3615add90a192cfd3c606e5c0b773797512b97
kaduvenancio1-cmyk <kaduvenancio1@gmail.com>
2026-01-23 16:09:02 -0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,112 +1,117 @@
 #import <UIKit/UIKit.h>
 
-static bool aim_ativo = false;
-static bool esp_ativo = false;
-static float fov_valor = 100.0f;
+// Definições de Memória (Simuladas para estabilidade)
+static bool ligar_aim = false;
+static bool ligar_esp = false;
+static float fov_dist = 100.0f;
 
-@interface RickzzMenu : UIView <UITableViewDelegate, UITableViewDataSource>
-@property (nonatomic, strong) UIView *container;
-@property (nonatomic, strong) UITableView *tabela;
-@property (nonatomic, strong) NSArray *itens;
-@property (nonatomic, strong) CAShapeLayer *fovLayer;
+@interface RickzzPainel : UIView <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tabelaMenu;
+@property (nonatomic, strong) CAShapeLayer *circuloFOV;
+@property (nonatomic, strong) NSArray *listaOpcoes;
 @end
 
-@implementation RickzzMenu
+@implementation RickzzPainel
 
 __attribute__((constructor))
-static void carregar_v24() {
+static void principal() {
+    // Delay de 15s para garantir que a engine do jogo já carregou
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *win = [[UIApplication sharedApplication] keyWindow];
+        UIWindow *w = [[UIApplication sharedApplication] keyWindow];
         
-        // GESTO: 3 Dedos, Toque Único (Fast)
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(abrir_menu:)];
-        tap.numberOfTouchesRequired = 3;
-        [win addGestureRecognizer:tap];
+        // GESTO: Toque rápido com 3 dedos para abrir/fechar
+        UITapGestureRecognizer *t = [[UITapGestureRecognizer alloc] initWithTarget:[RickzzPainel class] action:@selector(mudarEstado:)];
+        t.numberOfTouchesRequired = 3;
+        [w addGestureRecognizer:t];
     });
 }
 
-+ (void)abrir_menu:(UITapGestureRecognizer *)g {
-    if (g.state == UIGestureRecognizerStateEnded) [self toggle];
-}
-
-+ (void)toggle {
-    UIWindow *win = [[UIApplication sharedApplication] keyWindow];
-    UIView *velho = [win viewWithTag:888];
-    if (velho) [velho removeFromSuperview];
-    else {
-        RickzzMenu *menu = [[RickzzMenu alloc] initWithFrame:win.bounds];
-        menu.tag = 888;
-        [win addSubview:menu];
++ (void)mudarEstado:(UITapGestureRecognizer *)g {
+    if (g.state == UIGestureRecognizerStateEnded) {
+        UIWindow *w = [[UIApplication sharedApplication] keyWindow];
+        UIView *v = [w viewWithTag:777];
+        if (v) v.hidden = !v.hidden;
+        else {
+            RickzzPainel *p = [[RickzzPainel alloc] initWithFrame:w.bounds];
+            p.tag = 777;
+            [w addSubview:p];
+        }
     }
 }
 
+// CORREÇÃO DO ERRO DESIGNATED INITIALIZER (Visto nos logs)
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.itens = @[@"Aimbot Pro", @"ESP Line", @"Tracer", @"Box 2D", @"Distancia", @"Cabeça", @"Peito", @"ATIVAR FOV", @"Ajustar FOV"];
-        
-        // CÍRCULO DO FOV (Aparece no centro da tela)
-        _fovLayer = [CAShapeLayer layer];
-        _fovLayer.strokeColor = [UIColor cyanColor].CGColor;
-        _fovLayer.fillColor = [UIColor clearColor].CGColor;
-        _fovLayer.lineWidth = 1.5;
-        _fovLayer.hidden = YES;
-        [self.layer addSublayer:_fovLayer];
+        self.listaOpcoes = @[@"Aimbot Pro", @"ESP Line", @"ESP Box", @"Tracer", @"Distancia", @"Head", @"Chest", @"ATIVAR FOV", @"Ajustar FOV"];
+        self.backgroundColor = [UIColor clearColor];
+        self.userInteractionEnabled = YES;
 
-        // DESIGN DO PAINEL
-        _container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 380, 240)];
-        _container.center = self.center;
-        _container.backgroundColor = [UIColor colorWithWhite:0 alpha:0.96];
-        _container.layer.cornerRadius = 14;
-        _container.layer.borderWidth = 1.2;
-        _container.layer.borderColor = [UIColor cyanColor].CGColor;
-        [self addSubview:_container];
+        // CAMADA DO FOV (VISUAL)
+        _circuloFOV = [CAShapeLayer layer];
+        _circuloFOV.strokeColor = [UIColor cyanColor].CGColor;
+        _circuloFOV.fillColor = [UIColor clearColor].CGColor;
+        _circuloFOV.lineWidth = 2.0;
+        _circuloFOV.hidden = YES;
+        [self.layer addSublayer:_circuloFOV];
 
-        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(5, 5, 370, 230)];
-        _tabela.backgroundColor = [UIColor clearColor];
-        _tabela.delegate = self; _tabela.dataSource = self;
-        _tabela.separatorStyle = 0;
-        [_container addSubview:_tabela];
+        // PAINEL DE CONTROLE
+        UIView *fundo = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 380, 250)];
+        fundo.center = self.center;
+        fundo.backgroundColor = [UIColor colorWithWhite:0 alpha:0.98];
+        fundo.layer.cornerRadius = 15;
+        fundo.layer.borderColor = [UIColor cyanColor].CGColor;
+        fundo.layer.borderWidth = 1.5;
+        [self addSubview:fundo];
+
+        _tabelaMenu = [[UITableView alloc] initWithFrame:CGRectMake(5, 10, 370, 230) style:UITableViewStylePlain];
+        _tabelaMenu.backgroundColor = [UIColor clearColor];
+        _tabelaMenu.delegate = self;
+        _tabelaMenu.dataSource = self;
+        _tabelaMenu.separatorStyle = UITableViewCellSeparatorStyleNone;
+        [fundo addSubview:_tabelaMenu];
     }
     return self;
 }
 
-- (void)atualizarFOV {
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:self.center radius:fov_valor startAngle:0 endAngle:2*M_PI clockwise:YES];
-    _fovLayer.path = path.CGPath;
+- (void)desenharFOV {
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:self.center radius:fov_dist startAngle:0 endAngle:2*M_PI clockwise:YES];
+    _circuloFOV.path = path.CGPath;
 }
 
-- (void)mudou:(UISwitch *)s {
-    if (s.tag == 0) aim_ativo = s.on;
-    if (s.tag == 1) esp_ativo = s.on;
+- (void)acaoSwitch:(UISwitch *)s {
+    if (s.tag == 0) ligar_aim = s.on;
+    if (s.tag == 1) ligar_esp = s.on;
     if (s.tag == 7) { 
-        _fovLayer.hidden = !s.on;
-        [self atualizarFOV];
+        _circuloFOV.hidden = !s.on;
+        [self desenharFOV];
     }
 }
 
-- (void)sliderMudar:(UISlider *)sl {
-    fov_valor = sl.value;
-    [self atualizarFOV];
+- (void)acaoSlider:(UISlider *)sl {
+    fov_dist = sl.value;
+    [self desenharFOV];
 }
 
-- (NSInteger)tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)s { return self.itens.count; }
+// MÉTODOS DA TABELA
+- (NSInteger)tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)s { return self.listaOpcoes.count; }
 
-- (UITableViewCell *)tableView:(UITableView *)t cellForRowAtIndexPath:(NSIndexPath *)ip {
-    UITableViewCell *c = [[UITableViewCell alloc] initWithStyle:0 reuseIdentifier:@"c"];
+- (UITableViewCell *)tableView:(UITableView *)t cellForRowAtIndexPath:(NSIndexPath *)p {
+    UITableViewCell *c = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     c.backgroundColor = [UIColor clearColor];
-    c.textLabel.text = self.itens[ip.row];
+    c.textLabel.text = self.listaOpcoes[p.row];
     c.textLabel.textColor = [UIColor whiteColor];
-    
-    if (ip.row == 8) { // LINHA DO SLIDER
-        UISlider *sl = [[UISlider alloc] initWithFrame:CGRectMake(0, 0, 130, 20)];
-        sl.minimumValue = 50; sl.maximumValue = 350; sl.value = fov_valor;
-        sl.tintColor = [UIColor cyanColor];
-        [sl addTarget:self action:@selector(sliderMudar:) forControlEvents:UIControlEventValueChanged];
+    c.selectionStyle = UITableViewCellSelectionStyleNone;
+
+    if (p.row == 8) { // SLIDER
+        UISlider *sl = [[UISlider alloc] initWithFrame:CGRectMake(0, 0, 120, 20)];
+        sl.minimumValue = 30; sl.maximumValue = 400; sl.value = fov_dist;
+        [sl addTarget:self action:@selector(acaoSlider:) forControlEvents:UIControlEventValueChanged];
         c.accessoryView = sl;
     } else {
         UISwitch *sw = [[UISwitch alloc] init];
-        sw.tag = ip.row; [sw addTarget:self action:@selector(mudou:) forControlEvents:64];
+        sw.tag = p.row;
+        [sw addTarget:self action:@selector(acaoSwitch:) forControlEvents:UIControlEventValueChanged];
         c.accessoryView = sw;
     }
     return c;
