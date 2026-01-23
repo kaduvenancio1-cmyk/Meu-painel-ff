702191b33ab63ebd8064f511b0fe7763ec98598c
kaduvenancio1-cmyk <kaduvenancio1@gmail.com>
2026-01-23 13:51:29 -0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,82 +1,105 @@
 #import <UIKit/UIKit.h>
 #import <Security/Security.h>
 
-static bool p_ativa = false;
-static bool e_ativa = false;
+// Variáveis de controle com nomes falsos para camuflagem
+static bool liga_puxada = false;
+static bool liga_linha = false;
 
 @interface RickzzMenu : UIView <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UIView *fundo;
 @property (nonatomic, strong) UITableView *tabela;
-@property (nonatomic, strong) NSArray *lista;
+@property (nonatomic, strong) NSArray *opcoes;
 @end
 
 @implementation RickzzMenu
 
-// LIMPEZA DE SEGURANÇA
-static void limpar_rastro() {
-    NSString *c = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/com.garena.msdk/guest.dat"];
-    [[NSFileManager defaultManager] removeItemAtPath:c error:nil];
+// LIMPEZA AUTOMÁTICA DE REGISTROS
+static void limparRastro() {
+    NSString *caminho = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/com.garena.msdk/guest.dat"];
+    [[NSFileManager defaultManager] removeItemAtPath:caminho error:nil];
+    SecItemDelete((__bridge CFDictionaryRef)@{(__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword});
 }
 
 __attribute__((constructor))
-static void stealth_load() {
-    limpar_rastro();
-    // DELAY DE 50 SEGUNDOS - Só carrega o gesto após você entrar na partida
+static void inicializar_v23() {
+    limparRastro();
+    
+    // DELAY DE 50 SEGUNDOS (O menu só nasce após você entrar no Contra Squad)
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(50 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *w = [[UIApplication sharedApplication] keyWindow];
-        UILongPressGestureRecognizer *g = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(handle:)];
-        g.numberOfTouchesRequired = 3;
-        g.minimumPressDuration = 2.0;
-        [w addGestureRecognizer:g];
+        UIWindow *janela = [[UIApplication sharedApplication] keyWindow];
+        UILongPressGestureRecognizer *gesto = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(abrir:)];
+        gesto.numberOfTouchesRequired = 3;
+        gesto.minimumPressDuration = 2.0;
+        [janela addGestureRecognizer:gesto];
     });
 }
 
-+ (void)handle:(UILongPressGestureRecognizer *)s { if (s.state == 1) [self toggle]; }
++ (void)abrir:(UILongPressGestureRecognizer *)g {
+    if (g.state == UIGestureRecognizerStateBegan) [self alternar];
+}
 
-+ (void)toggle {
-    UIWindow *w = [[UIApplication sharedApplication] keyWindow];
-    UIView *old = [w viewWithTag:999];
-    if (old) [old removeFromSuperview];
++ (void)alternar {
+    UIWindow *win = [[UIApplication sharedApplication] keyWindow];
+    UIView *antigo = [win viewWithTag:999];
+    if (antigo) [antigo removeFromSuperview];
     else {
-        RickzzMenu *m = [[RickzzMenu alloc] initWithFrame:w.bounds];
-        m.tag = 999; [w addSubview:m];
+        RickzzMenu *menu = [[RickzzMenu alloc] initWithFrame:win.bounds];
+        menu.tag = 999;
+        [win addSubview:menu];
     }
 }
 
+// CORREÇÃO DO ERRO NS_DESIGNATED_INITIALIZER
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.lista = @[@"Puxada Pro", @"Linha ESP", @"Tracer", @"Box 2D", @"Metros", @"Head", @"Chest", @"ATIVAR FOV"];
-        UIView *bg = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 420, 260)];
-        bg.center = self.center;
-        bg.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.98];
-        bg.layer.cornerRadius = 15;
-        bg.layer.borderWidth = 1.0;
-        bg.layer.borderColor = [UIColor cyanColor].CGColor;
-        [self addSubview:bg];
+        self.opcoes = @[@"Puxada Pro", @"Linha ESP", @"Tracer", @"Box 2D", @"Metros", @"Head", @"Chest", @"ATIVAR FOV"];
+        
+        _fundo = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 420, 260)];
+        _fundo.center = self.center;
+        _fundo.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.98];
+        _fundo.layer.cornerRadius = 15;
+        _fundo.layer.borderWidth = 1.0;
+        _fundo.layer.borderColor = [UIColor cyanColor].CGColor;
+        [self addSubview:_fundo];
 
-        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(5, 40, 410, 180)];
+        // CORREÇÃO DA INICIALIZAÇÃO DA TABELA
+        _tabela = [[UITableView alloc] initWithFrame:CGRectMake(5, 40, 410, 180) style:UITableViewStylePlain];
         _tabela.backgroundColor = [UIColor clearColor];
-        _tabela.delegate = self; _tabela.dataSource = self;
-        _tabela.separatorStyle = 0;
-        [bg addSubview:_tabela];
+        _tabela.delegate = self;
+        _tabela.dataSource = self;
+        _tabela.separatorStyle = UITableViewCellSeparatorStyleNone;
+        [_fundo addSubview:_tabela];
     }
     return self;
 }
 
-- (void)changed:(UISwitch *)s {
-    if (s.tag == 0) p_ativa = s.on;
-    if (s.tag == 1) e_ativa = s.on;
+- (void)mudouSwitch:(UISwitch *)s {
+    if (s.tag == 0) liga_puxada = s.on;
+    if (s.tag == 1) liga_linha = s.on;
+    // Outras funções ativam aqui
 }
 
-- (NSInteger)tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)s { return self.lista.count; }
-- (UITableViewCell *)tableView:(UITableView *)t cellForRowAtIndexPath:(NSIndexPath *)p {
-    UITableViewCell *c = [[UITableViewCell alloc] initWithStyle:0 reuseIdentifier:@"c"];
+- (NSInteger)tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)s {
+    return self.opcoes.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)t cellForRowAtIndexPath:(NSIndexPath *)i {
+    static NSString *cellId = @"OpcaoCell";
+    UITableViewCell *c = [t dequeueReusableCellWithIdentifier:cellId];
+    if (!c) {
+        c = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    
     c.backgroundColor = [UIColor clearColor];
-    c.textLabel.text = self.lista[p.row];
+    c.textLabel.text = self.opcoes[i.row];
     c.textLabel.textColor = [UIColor whiteColor];
-    UISwitch *s = [[UISwitch alloc] init];
-    s.tag = p.row; [s addTarget:self action:@selector(changed:) forControlEvents:64];
-    c.accessoryView = s;
+    
+    UISwitch *sw = [[UISwitch alloc] init];
+    sw.tag = i.row;
+    [sw addTarget:self action:@selector(mudouSwitch:) forControlEvents:UIControlEventValueChanged];
+    c.accessoryView = sw;
+    
     return c;
 }
 @end
