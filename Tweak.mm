1abe037065d355181035c72fe6826089165fed43
kaduvenancio1-cmyk <kaduvenancio1@gmail.com>
2026-01-23 04:04:47 -0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,17 +1,39 @@
 #import <UIKit/UIKit.h>
+#import <Security/Security.h>
 
 @interface RickzzMenu : UIView <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UIView *container;
 @property (nonatomic, strong) UITableView *tabela;
 @property (nonatomic, strong) NSArray *itens;
 @property (nonatomic, strong) UISlider *fovSlider;
-@property (nonatomic, assign) BOOL fovAtivo;
 @end
 
 @implementation RickzzMenu
 
+// LIMPEZA PROFUNDA DE CONTA (GUEST RESET)
+static void reset_guest_account() {
+    // 1. Apaga todas as chaves de segurança do jogo (Keychain)
+    NSArray *secClasses = @[(__bridge id)kSecClassGenericPassword, (__bridge id)kSecClassInternetPassword, (__bridge id)kSecClassCertificate, (__bridge id)kSecClassKey, (__bridge id)kSecClassIdentity];
+    for (id secClass in secClasses) {
+        NSDictionary *spec = @{(__bridge id)kSecClass: secClass};
+        SecItemDelete((__bridge CFDictionaryRef)spec);
+    }
+    
+    // 2. Apaga arquivos de identificação na pasta Documents
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSString *docPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    NSArray *files = @[@"com.garena.msdk", @"device_id.txt", @"guest_info.json"];
+    for (NSString *file in files) {
+        [fm removeItemAtPath:[docPath stringByAppendingPathComponent:file] error:nil];
+    }
+    NSLog(@"[Rickzz] Limpeza profunda realizada.");
+}
+
 __attribute__((constructor))
 static void init_pro() {
+    // Executa a limpeza assim que o jogo abre
+    reset_guest_account();
+
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = [[UIApplication sharedApplication] keyWindow];
         UILongPressGestureRecognizer *segurar = [[UILongPressGestureRecognizer alloc] initWithTarget:[RickzzMenu class] action:@selector(aoSegurar:)];
@@ -41,7 +63,6 @@ static void init_pro() {
     if (self) {
         self.itens = @[@"Aimbot", @"ESP", @"Tracer", @"Skeleton", @"Distancia", @"Caixa 2D", @"Cabeça", @"Peito", @"Alerta Spect", @"Anti-Screenshot", @"Modo Streamer", @"ATIVAR/DESATIVAR FOV"];
         
-        // PAINEL DEITADO (Horizontal)
         _container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 450, 280)];
         _container.center = self.center;
         _container.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.95];
@@ -51,7 +72,7 @@ static void init_pro() {
         [self addSubview:_container];
 
         UILabel *titulo = [[UILabel alloc] initWithFrame:CGRectMake(0, 5, 450, 25)];
-        titulo.text = @"RICKZZ PRO V6 - FF 1.120.11";
+        titulo.text = @"RICKZZ PRO V7 - DEEP CLEAN";
         titulo.textColor = [UIColor cyanColor];
         titulo.textAlignment = NSTextAlignmentCenter;
         titulo.font = [UIFont boldSystemFontOfSize:16];
@@ -67,28 +88,18 @@ static void init_pro() {
         _fovSlider = [[UISlider alloc] initWithFrame:CGRectMake(50, 220, 350, 30)];
         _fovSlider.minimumValue = 30;
         _fovSlider.maximumValue = 150;
-        _fovSlider.hidden = YES; // Começa escondido
+        _fovSlider.hidden = YES;
         [_container addSubview:_fovSlider];
-        
-        UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(0, 250, 450, 20)];
-        l.text = @"AJUSTE FOV";
-        l.textColor = [UIColor grayColor];
-        l.font = [UIFont systemFontOfSize:9];
-        l.textAlignment = NSTextAlignmentCenter;
-        [_container addSubview:l];
     }
     return self;
 }
 
 - (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)s { return self.itens.count; }
-
 - (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)ip {
     UITableViewCell *c = [tv dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"c"];
     c.backgroundColor = [UIColor clearColor];
     c.textLabel.text = self.itens[ip.row];
     c.textLabel.textColor = [UIColor whiteColor];
-    c.textLabel.font = [UIFont systemFontOfSize:14];
-    
     UISwitch *s = [[UISwitch alloc] init];
     s.tag = ip.row;
     s.transform = CGAffineTransformMakeScale(0.7, 0.7);
@@ -98,20 +109,8 @@ static void init_pro() {
 }
 
 - (void)mudouSwitch:(UISwitch *)sender {
-    NSString *opt = self.itens[sender.tag];
-    
-    if ([opt isEqualToString:@"Cabeça"] && sender.on) [self desmarcar:7];
-    if ([opt isEqualToString:@"Peito"] && sender.on) [self desmarcar:6];
-    
-    // MOSTRAR/ESCONDER SLIDER DO FOV
-    if ([opt isEqualToString:@"ATIVAR/DESATIVAR FOV"]) {
+    if ([self.itens[sender.tag] isEqualToString:@"ATIVAR/DESATIVAR FOV"]) {
         _fovSlider.hidden = !sender.on;
     }
-}
-
-- (void)desmarcar:(NSInteger)i {
-    NSIndexPath *p = [NSIndexPath indexPathForRow:i inSection:0];
-    UITableViewCell *c = [self.tabela cellForRowAtIndexPath:p];
-    [(UISwitch *)c.accessoryView setOn:NO animated:YES];
 }
 @end
